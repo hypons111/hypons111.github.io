6c2434acfb37dd226533b191a173284750382e4b
kin <kin@topctek.com>
2024-04-10 18:15:12 +0800

    added publicPath in vue.config.js for github page.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/' // 指定應用中所有資源的基本路徑。確保應用能夠正確地找到其依賴的靜態資源（如 JavaScript 檔、CSS 檔以及圖片等）
 })
